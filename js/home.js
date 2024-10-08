1eabc1a9b5376befe1b86d0dfdb8fb156b88935c
Jesus Pedro Bapolo <145621406+Bapolo@users.noreply.github.com>
2024-10-09 00:08:08 +0100

    add Jquery for toggle menu

--- js/home.js
+++ js/home.js
@@ -1,47 +1,71 @@
-document.addEventListener("DOMContentLoaded", () =>
-{
-    const btnMenu = document.querySelector('.toggleButton')
-    const menu = document.querySelector('.menu')
-    const iconeDoBotao = document.querySelector('#icone')
-
-    btnMenu.addEventListener('click', () =>
+$(document).ready(function()
+{      
+    $('.toggleButton').click(function(event)
     {
-        abrirOuFecharMenu()       
+        //evita que o evento se propague para outras areas da DOM
+        event.stopPropagation()
+        $('.menu').fadeToggle(500)
     })
-
-    btnMenu.addEventListener('touchend', () =>
-    {
-        abrirOuFecharMenu()
-    })
-
-    function abrirOuFecharMenu()
-    {
-        if (menu.classList.contains('menuFechado'))
-            {
-                menu.classList.remove('menuFechado') 
-                menu.classList.add('menuAberto')
-                
-                setTimeout(() =>
-                {
-                    menu.style.opacity = '1'
-                }, 10)
-                            
-                iconeDoBotao.classList.remove('fa-bars')
-                iconeDoBotao.classList.add('fa-caret-down')         
-            }
-            else
-            {
-                menu.style.opacity = '0'
-    
-                setTimeout(() =>
-                {
-                    menu.classList.remove('menuAberto') 
-                menu.classList.add('menuFechado')
-                }, 500)
-    
-                iconeDoBotao.classList.remove('fa-caret-down')
-                iconeDoBotao.classList.add('fa-bars')
-                  
-            }
-    }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", () =>
+// {
+//     const btnMenu = document.querySelector('.toggleButton')
+//     const menu = document.querySelector('.menu')
+//     const iconeDoBotao = document.querySelector('#icone')
+
+//     btnMenu.addEventListener('click', () =>
+//     {
+//         abrirOuFecharMenu()       
+//     })
+
+//     btnMenu.addEventListener('touchend', () =>
+//     {
+//         abrirOuFecharMenu()
+//     })
+
+//     function abrirOuFecharMenu()
+//     {
+//         menu.classList.toggle('menuFechado')
+//     }
+// })
